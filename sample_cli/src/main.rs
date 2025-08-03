b139e1b8a561ff30a91d1f58201e42b3416303a4
yankee <tad.training2023@gmail.com>
2025-08-03 23:29:54 +0900

    逆ポーランド記法の計算ロジックの作成

--- sample_cli/src/main.rs
+++ sample_cli/src/main.rs
@@ -17,6 +17,53 @@ struct Opts {
     formula_file: Option<String>,
 }
 
+/// 逆ポーランド記法を計算する構造体
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn evaluate(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.evaluate_inner(&mut tokens)
+    }
+
+    fn evaluate_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let result = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(result);
+            }
+
+            // -vオプションが指定されていたら、この時点でのトークンとスタックを出力
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
+
 fn main() {
     let opts = Opts::parse();
 
@@ -31,9 +78,16 @@ fn main() {
     }
 }
 
-fn run<R: BufRead>(reader: R, _verbose: bool) {
+/// Runs the RPN calculator with the provided reader
+/// # Arguments:
+/// * `reader`: A reader that provides lines of input
+/// * `verbose`: A boolean indicating if verbose output is enabled
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.evaluate(&line);
+        println!("{}", answer);
     }
 }
