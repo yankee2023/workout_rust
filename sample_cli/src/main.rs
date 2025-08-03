93f2ece88a8bd3c17ed2e81743b831a912b0d200
yankee <tad.training2023@gmail.com>
2025-08-03 16:47:41 +0900

    アプリケーションの枠組みの作成

--- sample_cli/src/main.rs
+++ sample_cli/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -23,11 +23,17 @@ fn main() {
     if let Some(path) = opts.formula_file {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
+        run(reader, opts.verbose);
     } else {
-        println!("No file is specified.");
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
+    }
+}
+
+fn run<R: BufRead>(reader: R, _verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
     }
 }
