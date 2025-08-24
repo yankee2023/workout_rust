594dd22133c94602bd7cd40f6e0201e7f13880cd
yankee <tad.training2023@gmail.com>
2025-08-24 17:11:06 +0900

    POSTリクエスト(add, delete)

--- todo_web/src/main.rs
+++ todo_web/src/main.rs
@@ -1,9 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, ResponseError, http::header};
 use r2d2::Pool;
 use thiserror::Error;
 use askama::Template;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -32,6 +43,20 @@ enum MyError {
 
 // actix_web::ResponseErrorをMyErrorに実装する。
 impl ResponseError for MyError {}
+
+#[post("/add")]
+async fn add_todo(params: web::Form<AddParams>, db: web::Data<r2d2::Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let connection = db.get()?;
+    connection.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther().header(header::LOCATION, "/").finish())
+}
+
+#[post("/delete")]
+async fn delete_todo(params: web::Form<DeleteParams>, db: web::Data<r2d2::Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let connection = db.get()?;
+    connection.execute("DELETE FROM todo WHERE id = ?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther().header(header::LOCATION, "/").finish())
+}
 
 // MyErrorはactix_web::ResponseErrorを実装しているので、
 // indexの戻り値にMyErrorを使うことができる。
@@ -67,7 +92,16 @@ async fn main() -> Result<(), actix_web::Error> {
     let connection = pool.get().expect("Failed to get the connection from pool.");
     connection.execute("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY AUTOINCREMENT, text TEXT NOT NULL)", params![],)
               .expect("Failed to create a table `todo`.");
-    HttpServer::new(move || App::new().service(index).data(pool.clone())).bind("0.0.0.0:8080")?.run().await?;
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
 
     Ok(())
 }
